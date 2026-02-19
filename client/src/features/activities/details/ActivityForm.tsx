fb1bc42484ff3874620c919997fd7b2ef0a5bdd6
Rafaelle <villegasrafaelle@gmail.com>
2026-02-19 22:24:51 +0800

    End of section 7

--- client/src/features/activities/details/ActivityForm.tsx
+++ client/src/features/activities/details/ActivityForm.tsx
@@ -1,60 +1,65 @@
 import { Box, Button, Paper, TextField, Typography } from "@mui/material";
 import type { FormEvent } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { useNavigate, useParams } from "react-router";
 
-type Props = {
-    activity?: Activity
-    closeForm: () => void
-}
+export default function ActivityForm() {
+    const { id } = useParams();
+    const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
+    const navigate = useNavigate();
 
-export default function ActivityForm({activity, closeForm} : Props) {
-    const { updateActivity, createActivity } = useActivities();
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
-        
+
         const formData = new FormData(event.currentTarget)
 
-        const data: {[key: string]: FormDataEntryValue} = {}
+        const data: { [key: string]: FormDataEntryValue } = {}
         formData.forEach((value, key) => {
             data[key] = value
         })
 
-        if(activity) {
+        if (activity) {
             data.id = activity.id;
             await updateActivity.mutateAsync(data as unknown as Activity)
-            closeForm()
-        }  else {
-            await createActivity.mutateAsync(data as unknown as Activity)
-            closeForm()
-        }      
+            navigate(`/activities/${activity.id}`)
+        } else {
+            createActivity.mutate(data as unknown as Activity, {
+                onSuccess: (id) => {
+                    navigate(`/activities/${id}`)
+                }
+            })
+        }
     }
 
-  return (
-    <Paper sx={{borderRadius:3, padding: 3}}>
-        <Typography variant='h5' gutterBottom color='primary'>
-            Create Activity
-        </Typography>
-        <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
-            <TextField label='Title' name='title' defaultValue={activity?.title} />
-            <TextField label='Description' name='description' multiline rows={3} defaultValue={activity?.description}/>
-            <TextField label='Category' name='category' defaultValue={activity?.category}/>
-            <TextField label='Date' type='date' name='date' 
-                defaultValue={activity?.date 
-                    ? new Date(activity.date).toISOString().split('T')[0]
-                    : new Date().toISOString().split('T')[0]} />
-            <TextField label='City' name='city' defaultValue={activity?.city}/>
-            <TextField label='Venue' name='venue' defaultValue={activity?.venue}/>
-            <Box display='flex' justifyContent='end' gap={3}>
-                <Button onClick={closeForm} color='inherit'>Cancel</Button>
-                <Button 
-                    type='submit' 
-                    color='success' 
-                    variant='contained'
-                    disabled={updateActivity.isPending || createActivity.isPending }
-                >Submit</Button>
-            </Box>
-        </Box>
-    </Paper>
 
-  )
+    if (isLoadingActivity) return <Typography>Loading activity...</Typography>
+
+    return (
+        <Paper sx={{ borderRadius: 3, padding: 3 }}>
+            <Typography variant='h5' gutterBottom color='primary'>
+                {activity ? 'Edit activity' : 'Create activity'}
+            </Typography>
+            <Box component='form' onSubmit={handleSubmit} display='flex' flexDirection='column' gap={3}>
+                <TextField label='Title' name='title' defaultValue={activity?.title} />
+                <TextField label='Description' name='description' multiline rows={3} defaultValue={activity?.description} />
+                <TextField label='Category' name='category' defaultValue={activity?.category} />
+                <TextField label='Date' type='date' name='date'
+                    defaultValue={activity?.date
+                        ? new Date(activity.date).toISOString().split('T')[0]
+                        : new Date().toISOString().split('T')[0]} />
+                <TextField label='City' name='city' defaultValue={activity?.city} />
+                <TextField label='Venue' name='venue' defaultValue={activity?.venue} />
+                <Box display='flex' justifyContent='end' gap={3}>
+                    <Button color='inherit'>Cancel</Button>
+                    <Button
+                        type='submit'
+                        color='success'
+                        variant='contained'
+                        disabled={updateActivity.isPending || createActivity.isPending}
+                    >Submit</Button>
+                </Box>
+            </Box>
+        </Paper>
+
+    )
 }
--- client/src/lib/hooks/useActivities.ts
+++ client/src/lib/hooks/useActivities.ts
@@ -1,57 +1,70 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import agent from "../api/agent";
 
-export const useActivities = () => {
+export const useActivities = (id?: string) => {
 
-const queryClient = useQueryClient()
+    const queryClient = useQueryClient()
 
-    const {data: activities, isPending} = useQuery({
+    const { data: activities, isPending } = useQuery({
         queryKey: ['activities'],
-        queryFn: async() => {
-        const response = await agent.get<Activity[]>('/activities');
-        return response.data
+        queryFn: async () => {
+            const response = await agent.get<Activity[]>('/activities');
+            return response.data
         }
-  })
+    })
 
-  const updateActivity = useMutation({
-    mutationFn: async (activity: Activity) => {
-        await agent.put('/activities', activity)
-    },
-    onSuccess: async () => {
-        await queryClient.invalidateQueries({
-            queryKey: ['activities']
-        })
+    const { data: activity, isLoading: isLoadingActivity } = useQuery({
+        queryKey: ['activities', id],
+        queryFn: async () => {
+            const response = await agent.get<Activity>(`/activities/${id}`)
+            return response.data
+        },
+        enabled: !!id
+
+    })
+
+    const updateActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            await agent.put('/activities', activity)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    const createActivity = useMutation({
+        mutationFn: async (activity: Activity) => {
+            const response = await agent.post('/activities', activity)
+            return response.data;
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+
+    const deleteActivity = useMutation({
+        mutationFn: async (id: string) => {
+            await agent.delete(`/activities/${id}`)
+        },
+        onSuccess: async () => {
+            await queryClient.invalidateQueries({
+                queryKey: ['activities']
+            })
+        }
+    })
+
+    return {
+        activities,
+        isPending,
+        updateActivity,
+        createActivity,
+        deleteActivity,
+        activity,
+        isLoadingActivity
     }
-  })
-
-  const createActivity = useMutation({
-    mutationFn: async (activity: Activity) => {
-        await agent.post('/activities', activity)
-    },
-    onSuccess: async () => {
-        await queryClient.invalidateQueries({
-            queryKey: ['activities']
-        })
-    }
-  })
-
-  
-  const deleteActivity = useMutation({
-    mutationFn: async (id: string) => {
-        await agent.delete(`/activities/${id}`)
-    },
-    onSuccess: async () => {
-        await queryClient.invalidateQueries({
-            queryKey: ['activities']
-        })
-    }
-  })
-
-  return {
-    activities,
-    isPending,
-    updateActivity,
-    createActivity,
-    deleteActivity
-  }
 }
