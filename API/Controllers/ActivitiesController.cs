722a653f8eaafe02fab7dd048390d1ee7e937601
Rafaelle <villegasrafaelle@gmail.com>
2026-03-05 05:01:21 +0800

    End of section 10

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -2,6 +2,7 @@ using Domain;
 using Microsoft.AspNetCore.Mvc;
 using Application.Activities.Queries;
 using Application.Activities.Commands;
+using Application.Activities.DTO;
 
 namespace API.Controllers
 {
@@ -16,29 +17,26 @@ namespace API.Controllers
         [HttpGet("{id}")]
         public async Task<ActionResult<Activity>> GetActivityDetail(string id)
         {
-            return await Mediator.Send(new GetActivityDetails.Query{Id = id});
+            return HandleResult(await Mediator.Send(new GetActivityDetails.Query { Id = id }));
+
         }
 
         [HttpPost]
-        public async Task<ActionResult<string>> CreateActivity(Activity activity)
+        public async Task<ActionResult<string>> CreateActivity(CreateActivityDto activityDto)
         {
-            return await Mediator.Send(new CreateActivity.Command{Activity = activity});
-        } 
+            return HandleResult(await Mediator.Send(new CreateActivity.Command { ActivityDto = activityDto }));
+        }
 
         [HttpPut]
-        public async Task<ActionResult> EditActivity(Activity activity)
+        public async Task<ActionResult> EditActivity(EditActivityDto activity)
         {
-            await Mediator.Send(new EditActivity.Command{Activity = activity});
-
-            return NoContent();
+            return HandleResult(await Mediator.Send(new EditActivity.Command { ActivityDto = activity }));
         }
 
         [HttpDelete("{id}")]
         public async Task<ActionResult> DeleteActiity(string id)
         {
-            await Mediator.Send(new DeleteActivity.Command{Id = id});
-            
-            return Ok();
-        }     
+            return HandleResult(await Mediator.Send(new DeleteActivity.Command { Id = id }));
+        }
     }
 }
---
+++ Application/Activities/DTO/CreateActivityDto.cs
@@ -0,0 +1,8 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Application.Activities.DTO;
+
+public class CreateActivityDto : BaseActivityDto
+{
+    
+}
